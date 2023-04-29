21a3a5f93358648b9a086430dec5958f3b0c5b8c
sms1875 <01audtjr@gmail.com>
2023-04-29 21:34:29 +0900

    feat:월페이퍼 리스트 구조 변경 월페이퍼가 2x2형식으로 보이도록 변경 모바일일 경우 잠금화면, 배경화면 버튼 생성 모바일 지원 안되는 월페이퍼 예외처리

--- lib/screen/blackdesert_wallpaper_screen.dart
+++ lib/screen/blackdesert_wallpaper_screen.dart
@@ -1,88 +1,91 @@
 import 'package:wallpaper/screen/image_list_paging_widget.dart';
 import 'dart:io';
-
 import 'package:async_wallpaper/async_wallpaper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wallpaper/notifier/wallpaper_notifier.dart';
 
 class BlackDesertWallpaperScreen extends StatelessWidget {
-  const BlackDesertWallpaperScreen({
-    super.key,
-  });
+  final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldMessengerKey,
       body: Consumer<WallpaperNotifier>(
         builder: (_, notifier, __) {
           if (notifier.isLoading) {
-            return const Center(child: CircularProgressIndicator());
+            return Center(child: CircularProgressIndicator());
           } else {
+            final isPlatformSupported = Platform.isAndroid || Platform.isIOS;
             final wallpapers = notifier.imageList.wallpapers;
             return Column(
               children: [
-                Expanded(child:ListView.builder(
-                  itemCount: wallpapers.length,
-                  itemBuilder: (context, index) {
-                    final wallpaper = wallpapers[index];
-                    return InkWell(
-                      onTap: () async {
-                        // 배경화면 설정 코드
-                      },
-                      child: Card(
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 9 / 16,
+                    ),
+                    itemCount: wallpapers.length,
+                    itemBuilder: (context, index) {
+                      final wallpaper = wallpapers[index];
+                      var url = isPlatformSupported ? wallpaper['attr-img_m'] : wallpaper['attr-img'];
+                      if (!url!.startsWith('http')) {
+                        url=wallpaper['src'];
+                      }
+                      return Card(
                         child: Column(
                           children: [
-                            Image.network(wallpaper['src']!),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () async{
-                                    if(Platform.isAndroid || Platform.isIOS){
-                                      final url = wallpaper['attr-img_m'] ?? wallpaper['attr-img'];
-                                      final result = await AsyncWallpaper.setWallpaper(url: url!,wallpaperLocation:AsyncWallpaper.LOCK_SCREEN);
-                                      ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(
-                                          content: Text(result
-                                              ? '잠금화면이 성공적으로 설정되었습니다!'
-                                              : '잠금화면 설정에 실패했습니다.'),
-                                        ),
-                                      );
-                                    }
-                                  },
-                                  child: Text('잠금화면'),
-                                ),
-                                ElevatedButton(
-                                  onPressed:  () async{
-                                    if(Platform.isAndroid || Platform.isIOS){
-                                      final url = wallpaper['attr-img_m'] ?? wallpaper['attr-img'];
-                                      final result = await AsyncWallpaper.setWallpaper(url: url!,wallpaperLocation:AsyncWallpaper.HOME_SCREEN);
-                                      ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(
-                                          content: Text(result
-                                              ? '배경화면이 성공적으로 설정되었습니다!'
-                                              : '배경화면 설정에 실패했습니다.'),
-                                        ),
-                                      );
-                                    }
-                                  },
-                                  child: Text('배경화면'),
-                                ),
-                              ],
-                            ),
+                            Expanded(child: Image.network(url!)),
+                            if (isPlatformSupported)
+                              url==wallpaper['src'] ? Text("모바일은 지원하지 않습니다"):_isPlatformSupportedWidget(wallpaper: url),
                           ],
                         ),
-                      ),
-                    );
-                  },
-                )),
+                      );
+                    },
+                  ),
+                ),
                 ImageListPagingWidget(),
               ],
             );
           }
         },
       ),
+    );
+  }
+
+  Widget _isPlatformSupportedWidget({required String wallpaper}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () async{
+            final result = await AsyncWallpaper.setWallpaper(url: wallpaper, wallpaperLocation:AsyncWallpaper.LOCK_SCREEN);
+            scaffoldMessengerKey.currentState?.showSnackBar(
+              SnackBar(
+                content: Text(result
+                    ? '잠금화면이 성공적으로 설정되었습니다!'
+                    : '잠금화면 설정에 실패했습니다.'),
+              ),
+            );
+          },
+          child: Text('잠금화면'),
+        ),
+        ElevatedButton(
+          onPressed: () async{
+            final result = await AsyncWallpaper.setWallpaper(url: wallpaper, wallpaperLocation:AsyncWallpaper.HOME_SCREEN);
+            scaffoldMessengerKey.currentState?.showSnackBar(
+              SnackBar(
+                content: Text(result
+                    ? '배경화면이 성공적으로 설정되었습니다!'
+                    : '배경화면 설정에 실패했습니다.'),
+              ),
+            );
+          },
+          child: Text('배경화면'),
+        ),
+      ],
     );
   }
 }
