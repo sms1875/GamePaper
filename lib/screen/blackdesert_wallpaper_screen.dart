bc42063947e54c51d82bfa1f1e31983666e01cb4
sms1875 <01audtjr@gmail.com>
2023-04-30 00:36:13 +0900

    feat:페이지버튼 기능추가
    하단 페이지 가운데정렬,
    페이지 이동시 스크롤 맨 위로 이동 기능추가

--- lib/screen/blackdesert_wallpaper_screen.dart
+++ lib/screen/blackdesert_wallpaper_screen.dart
@@ -5,7 +5,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wallpaper/notifier/wallpaper_notifier.dart';
 
-class BlackDesertWallpaperScreen extends StatelessWidget {
+class BlackDesertWallpaperScreen extends StatefulWidget {
+  @override
+  State<BlackDesertWallpaperScreen> createState() => _BlackDesertWallpaperScreenState();
+}
+
+class _BlackDesertWallpaperScreenState extends State<BlackDesertWallpaperScreen> {
+  final _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     final notifier = context.watch<WallpaperNotifier>();
@@ -18,56 +30,53 @@ class BlackDesertWallpaperScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: _buildWallpaperGridView(wallpapers, isPlatformMobile),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 9 / 16,
+              ),
+              itemCount: wallpapers.length,
+              itemBuilder: (context, index) {
+                final wallpaper = wallpapers[index];
+                var url = isPlatformMobile ? wallpaper['attr-img_m'] : wallpaper['attr-img'];
+                //모바일이 지원 안되는 월페이퍼 구분
+                if (!url!.startsWith('http')) {
+                  url = wallpaper['src'];
+                }
+                return Card(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: InkWell(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (_) => Dialog(
+                                child: InkWell(
+                                  onTap: () => Navigator.pop(context),
+                                  child: Image.network(url!),
+                                ),
+                              ),
+                            );
+                          },
+                          child: Image.network(url!),
+                        ),
+                      ),
+                      isPlatformMobile
+                          ? url == wallpaper['src']
+                          ? Text("모바일은 지원하지 않습니다.")
+                          : _buildPlatformMobileWidget(context, url)
+                          : _buildPlatformDesktopWidget(context, url),
+                    ],
+                  ),
+                );
+              },
+              controller: _scrollController, // ScrollController 할당
+            ),
           ),
           _buildPageNumbers(context, pageUrls, currentPage),
         ],
       ),
-    );
-  }
-
-  Widget _buildWallpaperGridView(List<dynamic> wallpapers, bool isPlatformMobile) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 9 / 16,
-      ),
-      itemCount: wallpapers.length,
-      itemBuilder: (context, index) {
-        final wallpaper = wallpapers[index];
-        var url = isPlatformMobile ? wallpaper['attr-img_m'] : wallpaper['attr-img'];
-        //모바일이 지원 안되는 월페이퍼 구분
-        if (!url!.startsWith('http')) {
-          url = wallpaper['src'];
-        }
-        return Card(
-          child: Column(
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (_) => Dialog(
-                        child: InkWell(
-                          onTap: () => Navigator.pop(context),
-                          child: Image.network(url!),
-                        ),
-                      ),
-                    );
-                  },
-                  child: Image.network(url!),
-                ),
-              ),
-              isPlatformMobile
-                  ? url == wallpaper['src']
-                  ? Text("모바일은 지원하지 않습니다.")
-                  : _buildPlatformMobileWidget(context, url)
-                  : _buildPlatformDesktopWidget(context, url),
-            ],
-          ),
-        );
-      },
     );
   }
 
@@ -77,40 +86,46 @@ class BlackDesertWallpaperScreen extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         IconButton(
-          onPressed: currentPage == 1 ? null : () => context.read<WallpaperNotifier>().prevPage(context.read()),
+          onPressed: currentPage == 1 ? null : () {
+            context.read<WallpaperNotifier>().prevPage(context.read());
+            _scrollController.jumpTo(0); // 스크롤 맨 위로 이동
+          },
           icon: const Icon(Icons.arrow_back_ios),
         ),
-        Expanded(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                ...pageNumbers.map((i) => GestureDetector(
-                  onTap: () => context.read<WallpaperNotifier>().fetchImageListPage(context.read(), i),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      '$i',
-                      style: TextStyle(
-                        color: currentPage == i ? Colors.blue : Colors.black,
-                        fontWeight: currentPage == i ? FontWeight.bold : FontWeight.normal,
-                        fontSize: 20,
-                      ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              ...pageNumbers.map((i) => GestureDetector(
+                onTap: () {
+                  context.read<WallpaperNotifier>().fetchImageListPage(context.read(), i);
+                  _scrollController.jumpTo(0);
+                  },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '$i',
+                    style: TextStyle(
+                      color: currentPage == i ? Colors.blue : Colors.black,
+                      fontWeight: currentPage == i ? FontWeight.bold : FontWeight.normal,
+                      fontSize: 20,
                     ),
                   ),
-                )),
-              ],
-            ),
+                ),
+              )),
+            ],
           ),
         ),
         IconButton(
-          onPressed: currentPage == pageUrls.length ? null : () => context.read<WallpaperNotifier>().nextPage(context.read()),
+          onPressed: currentPage == pageUrls.length ? null : () {
+            context.read<WallpaperNotifier>().nextPage(context.read());
+            _scrollController.jumpTo(0); // 스크롤 맨 위로 이동
+          },
           icon: const Icon(Icons.arrow_forward_ios),
         ),
       ],
     );
   }
-
 
   Widget _buildPlatformMobileWidget(BuildContext context, String wallpaper) {
     return Row(
