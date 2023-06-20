725522450774ce092741b02cb68b8d97e8d51dd1
sms1875 <01audtjr@gmail.com>
2023-06-21 02:04:51 +0900

    refactor : MapleStory2WallpaperRepository 수정
    '배경화면 다운로드' 로 키워드 변경
    fetchWallpaperInfoFromSearchResult를 통한 멀티스레딩 및 redirect, 예외 처리
    한 게시글에서 여러 월페이퍼가 있을경우 처리

--- lib/repository/maplestory2_wallpaper_repository.dart
+++ lib/repository/maplestory2_wallpaper_repository.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 import 'package:wallpaper/repository/abstract_wallpaper_repository.dart';
 
 class MapleStory2WallpaperRepository extends AbstractWallpaperRepository {
   MapleStory2WallpaperRepository()
-      : super('https://maplestory2.nexon.com/Main/SearchNewsCategory?page=1&tk=%5B갤럭시%5D&t=media');
+      : super('https://maplestory2.nexon.com/Main/SearchNews?tk=배경화면%20다운로드');
 
   @override
   List<String> parsePaging(response) {
@@ -15,14 +16,14 @@ class MapleStory2WallpaperRepository extends AbstractWallpaperRepository {
         .map((a) => a.attributes['href']!)
         .toList();
 
-    //1페이지 추가
-    pages.insert(0,"/Main/SearchNewsCategory?page=1&tk=%5B갤럭시%5D&t=media");
+    // 1페이지 추가
+    pages.insert(0,"/Main/SearchNews?tk=배경화면%20다운로드");
     return pages;
   }
 
   @override
   List<List<String>> generatePageUrlsList(List<String> paging) {
-    //[[$page=1], [$page=2], ...]
+    // [[$page=1], [$page=2], ...]
     int pageSize = 1;
     List<List<String>> pageUrlsList = List.generate(
       (paging.length / pageSize).ceil(),
@@ -38,34 +39,73 @@ class MapleStory2WallpaperRepository extends AbstractWallpaperRepository {
     if (response.statusCode == 200) {
       final document = parse(response.body);
 
-      //검색 결과 게시글 목록
-      final searchResults=document
+      // 검색 결과 게시글 목록
+      final searchResults = document
           .querySelector('.result_sec')!
           .querySelectorAll('a')
           .map((a) => a.attributes['href']!);
 
-      //검색 결과의 각 게시글 href 링크로 들어가서 갤럭시 월페이퍼 링크를 가져옴
-      final wallpaperInfoFutures = searchResults.map((p) async {
-        final response2 = await http.get(Uri.parse('https://maplestory2.nexon.com$p'));
-        if (response2.statusCode == 200) {
-          final document2 = parse(response2.body);
+      // 검색 결과의 각 게시글 href 링크로 들어가서 갤럭시 월페이퍼 링크를 가져옴
+      final wallpaperInfoFutures = searchResults.map((p) => compute(fetchWallpaperInfoFromSearchResult, p));
+      final wallpaperInfoList = await Future.wait(wallpaperInfoFutures.toList());
 
-          final galaxyHref = document2
-              .querySelector('.board_view_body')!
-              .querySelectorAll('a')
-              .map((a) => a.attributes['href']!)
-              .where((href) => href.contains('galaxy'));
+      // fetchWallpaperInfoFromSearchResult를 통해 받아온 wallpaperInfoList 정보를 wallpapers에 더하기
+      List<Map<String, String>> wallpapers = [];
+      for (var wallpaperList in wallpaperInfoList) {
+        wallpapers.addAll(wallpaperList);
+      }
 
-          final hrefMap = await Future.wait(galaxyHref.map((href) => fetchWallpaperInfo(href)));
-          return hrefMap.reduce((mergedMap, currentMap) => mergedMap..addAll(currentMap));
-        } else {
-          throw Exception('Failed to load maplestory2 wallpaper');
-        }
-      });
-      final wallpapers = await Future.wait(wallpaperInfoFutures.toList());
+      // 빈 값이나 예외처리된 월페이퍼 제거
+      wallpapers.removeWhere((element) => element['url'] == '' || element.isEmpty);
+
       return wallpapers;
     } else {
       throw Exception('Failed to load maplestory2 wallpaper search result');
+    }
+  }
+
+  // 멀티쓰레딩을 위한 함수 분리
+  Future<List<Map<String, String>>> fetchWallpaperInfoFromSearchResult(String p) async {
+    final client = http.Client();
+    var uri = Uri.parse('https://maplestory2.nexon.com$p');
+    var request = http.Request('GET', uri);
+    request.followRedirects = false;
+    var response = await client.send(request);
+    while (response.isRedirect) {
+      response.stream.drain();
+      final location = response.headers['location'];
+      if (location != null) {
+        uri = Uri.parse(location);
+        request = http.Request('GET', uri);
+        request.followRedirects = false;
+        response = await client.send(request);
+      }
+    }
+
+    if (response.statusCode == 200) {
+      final responseBody = await response.stream.bytesToString();
+      final document = parse(responseBody);
+
+      // redirect 된 경우 다시 fetchWallpaperInfoFromSearchResult 함수를 호출
+      if (document.querySelector('.board_view_body') == null) {
+        return await fetchWallpaperInfoFromSearchResult(p);
+      }
+
+      final galaxyHref = document
+          .querySelector('.board_view_body')!
+          .querySelectorAll('a')
+          .map((a) => a.attributes['href']!)
+          .where((href) => href.contains('galaxy'))
+          .toList();
+
+      // 갤럭시 월페이퍼가 없는 경우 예외처리
+      if (galaxyHref.isEmpty) galaxyHref.add('');
+
+      final wallpaperInfoFutures = galaxyHref.map((href) => fetchWallpaperInfo(href));
+      final wallpaperInfoList = await Future.wait(wallpaperInfoFutures.toList());
+      return wallpaperInfoList;
+    } else {
+      throw Exception('Failed to load maplestory2 wallpaper');
     }
   }
 
