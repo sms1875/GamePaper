97ca8ac54ae0ec3149f5f4cde76620f2880f2dc3
송명석 <01audtjr@gmail.com>
2024-07-25 20:56:08 +0900

    Initial commit

---
+++ src/main/java/io/github/sms1875/gamepaper/GamepaperApplication.java
@@ -0,0 +1,13 @@
+package io.github.sms1875.gamepaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepaperApplication.class, args);
+	}
+
+}
