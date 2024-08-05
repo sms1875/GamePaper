2730cc7816f9133a777a2d2375112e10286db952
송명석 <01audtjr@gmail.com>
2024-08-06 00:25:46 +0900

    feat: .env 설정
    - flutter_dotenv 패키지 추가
    - .env 파일 생성 및 Firebase 설정 추가
    - main.dart 파일 수정하여 .env 파일 로드
    - firebase_options.dart 파일 수정하여 .env 값 사용
    - pubspec.yaml 파일에 assets 추가
    - 사용하지 않은 assets 이미지 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:wallpaper/screens/home_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  await dotenv.load(fileName: 'assets/config/.env');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
