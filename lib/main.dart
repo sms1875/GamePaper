6815d120c200dc4777e76a236d880ac751e955b7
송명석 <01audtjr@gmail.com>
2024-08-09 02:24:20 +0900

    feat: firebase 인증 연결
    firebase_auth, firebase_app_check 설정
    사용하지 않는 스크래핑 관련 라이브러리 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,19 +16,36 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Activate Firebase App Check
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.debug,
+  );
+
+  // Ensure the user is signed in
+  await _ensureAuthenticated();
+
   final gameRepository = GameRepository(FirebaseStorage.instance);
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => HomeProvider(gameRepository)),
       ],
-      child: MyApp(),
+      child: const MyApp(),
     ),
   );
 }
 
+Future<void> _ensureAuthenticated() async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  User? user = auth.currentUser;
+
+  user ??= (await auth.signInAnonymously()).user;
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
